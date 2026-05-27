6cbe0aaa6c408271c2923a6bd4c543088af846f9
dhruvpatel-sutariya <dhruvpatel90165@gmail.com>
2026-05-27 17:52:31 +0530

    fix: smtp port 587 for email

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,7 +6,9 @@ const { protect, adminOnly } = require('../middleware/authMiddleware');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
 });
 
